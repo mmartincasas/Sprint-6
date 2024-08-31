5f4ae165022b72f0e07a6ab27aaa6c223f5bec3f
Miriam Martin Casas <miriam.martin.casas@gmail.com>
2024-08-31 20:55:56 +0200

    revision

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,26 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import Budget from '../../models/budget';
 import BudgetClient from '../../models/budgetClient';
-import { FormBuilder, FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { PanelComponent } from '../panel/panel.component';
 import { BudgetListComponent } from "../budget-list/budget-list.component";
 import { BudgetService } from '../../services/budget.service';
 import { isPhoneNumber } from '../../validators/number-validators';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ReactiveFormsModule, PanelComponent, BudgetListComponent],
+  imports: [ReactiveFormsModule, PanelComponent, BudgetListComponent, FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
   public formBudget: FormGroup;
   public formClientBudget: FormGroup;
 
-  public budget: Budget = {
+  protected budget: Budget = {
     seo: false,
     ads: false,
     web: false,
@@ -32,9 +34,11 @@ export class HomeComponent implements OnInit {
   public submitBudget: boolean = false;
   public errorSubmitBudget: boolean = false;
 
-
-
-  constructor(protected budgetService: BudgetService, private fb: FormBuilder) {
+  constructor(
+    protected budgetService: BudgetService, 
+    private fb: FormBuilder,
+    private router: Router,
+    private activatedRoute: ActivatedRoute) {
 
     this.formBudget = this.fb.group({
       seo: [this.budget.seo],
@@ -47,17 +51,32 @@ export class HomeComponent implements OnInit {
       phone: ['', [Validators.required, isPhoneNumber()]],
       email: ['', [Validators.required, Validators.email]]
     });
-
   }
 
   ngOnInit(): void {
     this.initFormBudget();
   }
 
-  initFormBudget() {
-    this.formBudget.valueChanges.subscribe(values => {
-      this.budget = { ...this.budget, ...values };
-      this.updateBudget();
+   
+  initBudgetFromURL(): Promise<void> {
+
+    return new Promise((resolve) => {
+      this.activatedRoute.queryParams.subscribe(params => {
+        this.budget.seo = params['Seo'] ? true : false;
+        this.budget.ads = params['Ads'] ? true : false;
+        this.budget.web = params['Web'] ? true : false;
+          
+        if (this.budget.web) {
+          const panelValues = {
+            pages: params['Pages'] ? +params['Pages'] : 0,
+            languages: params['Lang'] ? +params['Lang'] : 0
+          };
+          this.setInfoPanel(panelValues);
+        }
+        console.log('INITBudgetFromURL:', this.budget);
+
+        resolve(); 
+      });
     });
   }
 
@@ -67,17 +86,82 @@ export class HomeComponent implements OnInit {
     this.updateBudget();
   }
 
+  async initFormBudget(){
+
+    console.log('1:',this.budget)
+
+    await this.initBudgetFromURL();
+
+    //EL SETTIMEOUT QUE HACE QUE FUNCIONE, LA PROMESA NO FUNCIONA!!!
+    /* */
+    setTimeout(() => {
+
+    console.log('2:',this.budget, this.budget.seo)
+    console.log('Contenido de this.budget en JSON:', JSON.stringify(this.budget));
+
+    this.formBudget = this.fb.group({
+      seo: [this.budget.seo], 
+      ads: [this.budget.ads], 
+      web: [this.budget.web]  
+    });
+
+    console.log('Valores de budget:');
+    console.log('SEO:', this.budget.seo);
+    console.log('Ads:', this.budget.ads);
+    console.log('Web:', this.budget.web);
+
+    console.log('Valores iniciales del formulario:');
+    console.log('SEO:', this.formBudget.get('seo')?.value);
+    console.log('Ads:', this.formBudget.get('ads')?.value);
+    console.log('Web:', this.formBudget.get('web')?.value);
+
+    this.formBudget.valueChanges.subscribe(values => {
+      this.budget = { ...this.budget, ...values };
+    });
+
+  }, 1000);
+    
+  }
+
   updateBudget(): void {
+
     if (!this.budget.web) {
       this.budget.pages = 0;
       this.budget.languages = 0;
+      
     }
+    
     this.budget.totalBudget = this.budgetService.calculateBudget(this.budget);
+
+    console.log('Saliendo de updateBudget', this.budget)
+    console.log('Contenido de this.budget en JSON:', JSON.stringify(this.budget));
+
+    this.updateUrlParams();
+
+  }
+
+
+  updateUrlParams(): void {
+    const params: any = {};
+
+    if (this.budget.seo === true) params.Seo = this.budget.seo;
+    if (this.budget.ads === true) params.Ads = this.budget.ads;
+    if (this.budget.web === true) params.Web = this.budget.web;
+    if (this.budget.pages > 0) params.Pages = this.budget.pages;
+    if (this.budget.languages > 0) params.Lang = this.budget.languages;
+      
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: params,
+      queryParamsHandling: 'replace'
+    });
+
+    console.log('UPDATEURLPARAMS')
+
   }
 
 
   createClientBudget() {
-
     this.formClientBudget.markAllAsTouched();
     
     if(this.formClientBudget.invalid||this.budget.totalBudget==0){
--- src/app/components/panel/panel.component.ts
+++ src/app/components/panel/panel.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+
+/*
+import { Component, EventEmitter, OnInit, Output, Input, ViewChild, OnChanges } from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule} from '@angular/forms'
 import { positiveIntegerValidator } from '../../validators/number-validators';
 import { ModalComponent } from '../modal/modal.component';
@@ -10,7 +12,10 @@ import { ModalComponent } from '../modal/modal.component';
   templateUrl: './panel.component.html',
   styleUrl: './panel.component.scss'
 })
-export class PanelComponent implements OnInit {
+export class PanelComponent implements OnInit, OnChanges {
+
+  @Input() pagesURL: number = 0;
+  @Input() langURL: number = 0;
 
   @Output()
   emitPanelValue: EventEmitter<any> = new EventEmitter <any>
@@ -23,10 +28,17 @@ export class PanelComponent implements OnInit {
     this.initFormWebPanel();
   }
 
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.pagesURL || changes.langURL) {
+      this.updateFormValues();
+    }
+  }
+
   initFormWebPanel(){
     this.formWebPanel = new FormGroup ({
-      pages: new FormControl (0, [positiveIntegerValidator()]),
-      languages: new FormControl (0, [positiveIntegerValidator()])
+      pages: new FormControl (this.pagesURL, [positiveIntegerValidator()]),
+      languages: new FormControl (this.langURL, [positiveIntegerValidator()])
     })
 
     this.formWebPanel.valueChanges.subscribe(() => {
@@ -66,4 +78,89 @@ export class PanelComponent implements OnInit {
     this.newModal.openModal(type);
   }
 
+}*/
+
+import { Component, EventEmitter, OnInit, Output, Input, ViewChild, OnChanges, SimpleChanges } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { positiveIntegerValidator } from '../../validators/number-validators';
+import { ModalComponent } from '../modal/modal.component';
+
+@Component({
+  selector: 'app-panel',
+  standalone: true,
+  imports: [ReactiveFormsModule, ModalComponent],
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.scss']
+})
+export class PanelComponent implements OnInit, OnChanges {
+
+  @Input() pagesURL: number = 0;
+  @Input() langURL: number = 0;
+
+  @Output()
+  emitPanelValue: EventEmitter<any> = new EventEmitter<any>();
+
+  @ViewChild(ModalComponent) newModal!: ModalComponent;
+
+  public formWebPanel: FormGroup = new FormGroup({});
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['pagesURL'] || changes['langURL']) {
+      this.updateFormValues();
+    }
+  }
+
+  ngOnInit(): void {
+    this.initFormWebPanel();
+  }
+
+  initFormWebPanel() {
+    this.formWebPanel = new FormGroup({
+      pages: new FormControl(this.pagesURL, [positiveIntegerValidator()]), // Inicializa con pagesURL
+      languages: new FormControl(this.langURL, [positiveIntegerValidator()]) // Inicializa con langURL
+    });
+
+    this.formWebPanel.valueChanges.subscribe(() => {
+      this.emitValidValues();
+    });
+  }
+
+  updateFormValues() {
+    if (this.formWebPanel) {
+      this.formWebPanel.patchValue({
+        pages: this.pagesURL,
+        languages: this.langURL
+      }, { emitEvent: false }); // No emitir evento para evitar bucles infinitos
+    }
+  }
+
+  emitValidValues() {
+    const pagesValue = this.formWebPanel.get('pages')?.value;
+    const languagesValue = this.formWebPanel.get('languages')?.value;
+
+    const validValues: any = {};
+   
+    validValues.pages = (this.formWebPanel.get('pages')?.valid && pagesValue >= 0) ? pagesValue : 0;
+    validValues.languages = (this.formWebPanel.get('languages')?.valid && languagesValue >= 0) ? languagesValue : 0;
+   
+    this.emitPanelValue.emit(validValues);
+  }
+
+  updatePanelValue(controlName: string, action: string) {
+    const currentNumber = this.formWebPanel.get(controlName)?.value ?? 0;
+
+    if (action === 'decrease' && currentNumber > 0) {
+      this.formWebPanel.get(controlName)?.setValue(currentNumber - 1);
+    } else if (action === 'increase') {
+      if (currentNumber < 0) {
+        this.formWebPanel.get(controlName)?.setValue(0);
+      } else {
+        this.formWebPanel.get(controlName)?.setValue(currentNumber + 1);
+      }
+    }
+  }
+
+  openModal(type: string) {
+    this.newModal.openModal(type);
+  }
 }
